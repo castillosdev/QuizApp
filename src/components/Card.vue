51d0a385c26dc0f5950b360525b850fd7bb0c551
Louis Castillo <louiscastillo.dev@outlook.com>
2023-11-22 17:20:29 -0500

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<script setup>
+    import { defineProps } from 'vue'
+    const {quizData} = defineProps(['quizData'])
+    console.log(quizData)
+</script>
+<template>
+      <div class="card">
+        <img :src="quizData.img" alt="">
+        <div class="card-text">
+          <h2>{{ quizData.name }}</h2>
+          <p>{{ quizData.questions.length }} questions</p>
+        </div>
+      </div>
+</template>
+<style scoped>
+    .card {
+        width: 310px;
+        overflow: hidden;
+        border-radius: 2%;
+        box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+        margin-bottom: 35px;
+        margin-right: 20px;
+        cursor: pointer;
+    }
+    
+    .card img {
+        width: 100%;
+        height: 190px;
+        margin: 0
+    }
+    
+    .card .card-text {
+        padding: 20px;
+    }
+    
+    .card .card-text h2 {
+        font-size: 20px;
+        margin-bottom: 10px;
+    }
+    
+    .card .card-text p {
+        font-size: 14px;
+        color: rgba(0,0,0,0.5);
+    }
+</style>
