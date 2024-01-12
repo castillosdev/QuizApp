cb1bab8215d23766e06e895760ad187d08007e66
Louis Castillo <lcastillo@riskprograms.com>
2024-01-12 15:20:41 -0500

    Refactor QuizView.vue component

---
+++ src/components/Question.vue
@@ -0,0 +1,60 @@
+<script setup>
+    import { defineProps } from 'vue'
+
+    const {question} = defineProps(['question'])
+</script>
+
+<template>
+  <div class="question-container">
+    <h1 class="question">{{ question.text }}</h1>
+  </div>
+  <div class="options-container">
+    <div v-for="option in question.options" :key="option.id" class="option">
+      <p class="option-label">{{ option.label }}</p>
+      <div class="option-value">
+        <p>{{ option.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+    .question {
+    font-size: 40px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    }
+
+    .option {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 20px;
+    cursor: pointer;
+    }
+
+    .option-label {
+    width: 60px;
+    height: 60px;
+    background-color: bisque;
+    font-size: 30px;
+    font-weight: bold;
+    margin-right: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: rgb(107, 107, 107);
+    }
+
+    .option-value {
+    width: 200px;
+    height: 60px;
+    color: rgb(107, 107, 107);
+    background-color: #f2f2f2;
+    font-size: 30px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
+</style>
---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h4>Question 1/3</h4>
+        <div class="bar">
+            <div class="completion">
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        margin-top: 20px;
+    }
+    header h4{
+        font-weight: bold;
+        font-size: 30px
+    }
+
+    .bar{
+        width: 300px;
+        height: 50px;
+        border-radius: 5px;
+        border: 3px solid #adfcb7;
+        margin-top: 10px;
+    }
+
+    .completion{
+        width: 0%;
+        height: 100%;
+        background-color: #008111;
+        border-radius: 5px;
+    }
+</style>
